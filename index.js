17282721e03a78ddd4ec2ef84f35582d32ec22a1
Bálint Csertán <csertanb@hotmail.com>
2022-01-17 20:20:08 +0100

    Shorting code

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const { createElement } = React
-const { render } = ReactDOM
-
-const style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'verdana'
-}
-
-const title = createElement(
-    'h1',
-    {id: 'title', className: 'header' , style: style},
-    'Hello World'
-)
+ const { render } = ReactDOM
 
 render(
-    title,
+    <h1 id="title" className="header" style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+        Hello World
+    </h1>,
     document.getElementById('react-container')
 )
