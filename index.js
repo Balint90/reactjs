29ddfcc4bbcdc3db3e5e83d1a6c08d219392c5fa
Bálint Csertán <csertanb@hotmail.com>
2022-01-17 22:37:24 +0100

    Add Babeljs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const { render } = ReactDOM
+const { render } = ReactDOM
 
 render(
     <h1 id="title" className="header" style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
